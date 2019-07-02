edec5abe5e3cba4ec9ab36daa331cb9fb1ef3eb1
hoideptrai123 <toilahoi007@gmail.com>
2019-07-02 20:38:16 +0700

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { IntroduceComponent } from './introduce/introduce.component';
+import { ContactComponent } from './contact/contact.component';
+import { MapComponent } from './map/map.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IntroduceComponent,
+    ContactComponent,
+    MapComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
